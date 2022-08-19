44b9f89a2bd5e41ee7a1a6bbdd349104cc86926a
elad <eladr@post.bgu.ac.il>
2022-08-19 09:09:18 +0300

    genres working

--- tfrs/tfrs_project.py
+++ tfrs/tfrs_project.py
@@ -1,3 +1,5 @@
+import argparse
+
 import numpy as np
 import tensorflow as tf
 import tensorflow_datasets as tfds
@@ -5,8 +7,9 @@ import tensorflow_recommenders as tfrs
 
 
 class UserModel(tf.keras.Model):
-    def __init__(self, n_unique_user_ids, embedding_size=32):
+    def __init__(self, n_unique_user_ids, embedding_size=32, additional_features=(), additional_feature_info=None):
         super().__init__()
+        self.additional_embeddings = {}
 
         self.user_embedding = tf.keras.Sequential([
             tf.keras.layers.StringLookup(
@@ -14,21 +17,37 @@ class UserModel(tf.keras.Model):
             tf.keras.layers.Embedding(n_unique_user_ids + 1, embedding_size)
         ])
 
+        if 'timestamp' in additional_features:
+            self.additional_embeddings['timestamp'] = tf.keras.Sequential([
+                tf.keras.layers.Discretization(additional_feature_info['timestamp_buckets'].tolist()),
+                tf.keras.layers.Embedding(len(additional_feature_info['timestamp_buckets']) + 1, 32),
+            ])
+
+        if 'movie_genres' in additional_features:
+            self.additional_embeddings['movie_genres'] = tf.keras.Sequential([
+                tf.keras.layers.Embedding(len(additional_feature_info['unique_movie_genres']) + 1, 32),
+                tf.keras.layers.Lambda(lambda x: tf.math.reduce_mean(x, axis=1))
+            ])
+
     def call(self, inputs):
-        return self.user_embedding(inputs['user_id'])
+        return tf.concat([self.user_embedding(inputs['user_id'])] +
+                         [self.additional_embeddings[k](inputs[k]) for k in self.additional_embeddings],
+                         axis=1)
 
 
 class MovieModel(tf.keras.Model):
     def __init__(self, unique_movie_titles, embedding_size=32):
         super().__init__()
+        self.additional_embeddings = {}
+
         self.title_embedding = tf.keras.Sequential([
             tf.keras.layers.StringLookup(
                 vocabulary=unique_movie_titles, mask_token=None),
             tf.keras.layers.Embedding(len(unique_movie_titles) + 1, embedding_size)
         ])
 
-    def call(self, titles):
-        return self.title_embedding(titles)
+    def call(self, inputs):
+        return self.title_embedding(inputs)
 
 
 class QueryCandidateModel(tf.keras.Model):
@@ -46,31 +65,53 @@ class QueryCandidateModel(tf.keras.Model):
 
 
 class MovieLensModel(tfrs.models.Model):
-    def __init__(self, layer_sizes, movies, unique_movie_titles, n_unique_user_ids):
+    def __init__(self, layer_sizes, movies, unique_movie_titles, n_unique_user_ids, additional_features,
+                 additional_feature_info):
         super().__init__()
-        self.query_model = QueryCandidateModel(layer_sizes, UserModel(n_unique_user_ids))
+        self.additional_features = additional_features
+        self.query_model = QueryCandidateModel(layer_sizes, UserModel(n_unique_user_ids,
+                                                                      additional_features=self.additional_features,
+                                                                      additional_feature_info=additional_feature_info))
         self.candidate_model = QueryCandidateModel(layer_sizes, MovieModel(unique_movie_titles))
         self.task = tfrs.tasks.Retrieval(
             metrics=tfrs.metrics.FactorizedTopK(
-                candidates=movies.batch(128).map(self.candidate_model),
+                candidates=(movies
+                            .batch(128)
+                            .map(self.candidate_model)),
             ),
         )
 
     def compute_loss(self, features, training=False):
         query_embeddings = self.query_model({
-            'user_id': features['user_id']
+            'user_id': features['user_id'],
+            **{k: features[k] for k in self.additional_features}
         })
         movie_embeddings = self.candidate_model(features['movie_title'])
         return self.task(query_embeddings, movie_embeddings, compute_metrics=not training)
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--num_epochs', type=int, default=10)
+    parser.add_argument('--embedding_size', type=int, default=32)
+    parser.add_argument('--layer_sizes', nargs='+', default=[32])
+    parser.add_argument('--additional_features', nargs='+', default=[], help='options: timestamp,')
+    args = parser.parse_args()
+
     movies = (tfds.load("movielens/100k-movies", split="train")
-              .map(lambda x: x["movie_title"]))
+              .map(lambda x: x['movie_title']))
     ratings = (tfds.load("movielens/100k-ratings", split="train")
                .shuffle(100_000, seed=42, reshuffle_each_iteration=False))
     unique_movie_titles = np.unique(np.concatenate(list(ratings.map(lambda x: x["movie_title"]).batch(1000))))
+    unique_movie_genres = np.unique(np.concatenate(list(ratings.map(lambda x: x["movie_genres"]).as_numpy_iterator())))
     unique_user_ids = np.unique(np.concatenate(list(ratings.map(lambda x: x["user_id"]).batch(1000))))
+
+    timestamps = np.concatenate(list(ratings.map(lambda x: x["timestamp"]).batch(100)))
+    max_timestamp = timestamps.max()
+    min_timestamp = timestamps.min()
+    additional_feature_info = {'timestamp_buckets': np.linspace(min_timestamp, max_timestamp, num=1000),
+                               'unique_movie_genres': unique_movie_genres}
+
     trainset = (ratings
                 .take(80_000)
                 .shuffle(100_000)
@@ -80,21 +121,23 @@ if __name__ == '__main__':
     testset = (ratings
                .skip(80_000)
                .take(20_000)
-               .apply(tf.data.experimental.dense_to_ragged_batch(4096))
+               .apply(tf.data.experimental.dense_to_ragged_batch(2048))
                .cache())
 
-    num_epochs = 300
-
-    model = MovieLensModel([32], movies, unique_movie_titles, len(unique_user_ids))
+    model = MovieLensModel(args.layer_sizes, movies, unique_movie_titles, len(unique_user_ids),
+                           additional_features=args.additional_features,
+                           additional_feature_info=additional_feature_info)
     model.compile(optimizer=tf.keras.optimizers.Adagrad(0.1), run_eagerly=True)
 
-    one_layer_history = model.fit(
+    model_history = model.fit(
         trainset,
         validation_data=testset,
-        validation_freq=5,
-        epochs=num_epochs,
+        validation_freq=3,
+        epochs=args.num_epochs,
         verbose=1)
 
-    accuracy = one_layer_history.history["val_factorized_top_k/top_100_categorical_accuracy"][-1]
-    print(f"Top-100 accuracy: {accuracy:.2f}.")
+    accuracy = model_history.history["val_factorized_top_k/top_100_categorical_accuracy"][-1]
+
+    print(f'Run settings: {vars(args)}')
+    print(f"Top-100 accuracy: {accuracy:.2f}")
 
